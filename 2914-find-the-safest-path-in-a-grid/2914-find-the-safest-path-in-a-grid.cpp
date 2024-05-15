d15ae6786fcc5ee6ed817a53c90271110bf6e7f9
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-05-15 22:53:29 +0530

    Time: 787 ms (33.22%), Space: 272.5 MB (20.44%) - LeetHub

---
+++ 2914-find-the-safest-path-in-a-grid/2914-find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,75 @@
+#define fl(i, l, r) for(int i= (l); i< (r); i++)
+#define rl(i, l, r) for(int i= (r-1); i>= (l); i--)
+#define pb push_back
+#define pp pop_back
+#define mk make_pair
+
+typedef long long ll;
+
+class Solution {
+    vector<vector<int>> manh;
+
+    vector<int> dirx = {1, 0, -1, 0};
+    vector<int> diry = {0, 1, 0, -1};
+    void bfs(vector<vector<int>>& grid){
+        int n = grid.size();
+        queue<vector<int>> q;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1){
+                    q.push({i, j});
+                    manh[i][j] = 0;
+                }
+            }
+        }
+        while(!q.empty()){
+            int x = q.front()[0];
+            int y = q.front()[1];
+            q.pop();
+            fl(i, 0, 4){
+                int nx = x+dirx[i];
+                int ny = y+diry[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<n && manh[nx][ny]==-1){
+                    manh[nx][ny] = manh[x][y]+1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+
+    int solve(vector<vector<int>>& grid){
+        int n = grid.size();
+        priority_queue<vector<int>> pq;
+        vector<vector<int>> safe(n, vector<int>(n, INT_MAX));
+        pq.push({manh[0][0], 0, 0});
+        safe[0][0] = manh[0][0];
+
+        while(!pq.empty()){
+            int x = pq.top()[1];
+            int y = pq.top()[2];
+            int safety = pq.top()[0];
+            pq.pop();
+            if(x == n-1 && y == n-1)
+                return safety;
+            fl(i, 0, 4){
+                int nx = x + dirx[i];
+                int ny = y + diry[i];
+                if(nx >=0 && nx < n && ny >= 0 && ny < n && safe[nx][ny] == INT_MAX){
+                    safe[nx][ny] = min(safety, manh[nx][ny]);
+                    pq.push({safe[nx][ny], nx, ny});
+                }
+            }
+        }
+        return -1;
+    }
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> temp(n, -1);
+        fl(i, 0, n)
+            manh.pb(temp);
+
+        bfs(grid);
+        return solve(grid);
+    }
+};
