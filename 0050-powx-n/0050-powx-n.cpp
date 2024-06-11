330c7faf287e63969986b515291f597d6ec1c0f3
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-06-12 00:23:05 +0530

    Time: 3 ms (37.26%), Space: 8 MB (73.33%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long nn = n;
+        if(n < 0) nn = -1 *nn;
+        double ans = 1.0;
+        while(nn){
+            if(nn % 2){
+                ans = ans * x;
+                nn--;
+            }
+            else{
+                x = x * x;
+                nn /= 2;
+            }
+        }
+
+        if( n< 0) return (1.0 / ans);
+        return ans;
+    }
+};
