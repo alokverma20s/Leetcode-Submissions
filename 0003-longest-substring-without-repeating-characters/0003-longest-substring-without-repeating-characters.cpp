209be569d0eae949d1daeb23aa7334e98becee12
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-06-16 03:19:13 +0530

    Time: 16 ms (50.93%), Space: 11.1 MB (69.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,21 +4,17 @@ public:
         if(s=="")
             return 0;
         int ans =1;
-        unordered_set<char> st;
+        unordered_map<char, int> st;
         int left = 0;
         int right = 0;
         int n = s.length();
 
         while( right < n){
-            if(st.find(s[right]) == st.end()){
-                ans = max(ans , right - left +1);
-            }else{
-                while(left < right && st.find(s[right]) !=st.end()){
-                    st.erase(s[left]);
-                    left++;
-                }
+            if(st.find(s[right]) != st.end() && st[s[right]] >= left){
+                left = st[s[right]]+1;
             }
-            st.insert(s[right]);
+            ans = max(ans , right - left +1);
+            st[s[right]] = right;
             right++;
         }
         return ans;
