3dfacedf0225fa24ecdb4961813f925505b63296
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-06-17 13:32:58 +0530

    Time: 36 ms (78.31%), Space: 17.2 MB (35.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,21 +9,31 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* d1 = headA;
-        unordered_set<ListNode*> st;
+        ListNode* d1 = headA, *d2 = headB;
+        int cnt1 = 0, cnt2 = 0;
+
+        while(d1 != NULL || d2 != NULL){
+            if(d1 != NULL){
+                cnt1++;
+                d1= d1->next;
+            }
+            if(d2 != NULL){
+                cnt2++;
+                d2 = d2->next;
+            }
+        }
+        int diff = abs(cnt1-cnt2);
+        cnt1 < cnt2 ? d1= headB : d1 = headA;
+        cnt1 < cnt2 ? d2= headA : d2 = headB;
+
+        while(diff--) d1 = d1->next;
 
         while(d1 != NULL){
-            st.insert(d1);
+            if(d1 == d2)
+                return d1;
             d1 = d1->next;
-        }
-
-        ListNode* d2 = headB;
-        while(d2 != NULL){
-            if(st.find(d2) != st.end())
-                return d2;
             d2 = d2->next;
         }
-
         return NULL;
     }
 };
