be137729fe8815462d08e69441badc82ff63bba6
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-09-02 12:01:38 +0530

    Time: 19 ms (23.82%), Space: 20.6 MB (71.42%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i: nums) ans ^= i;
+        for(int i=0; i<= nums.size(); i++) ans ^= i;
+        return ans;
+    }
+};
