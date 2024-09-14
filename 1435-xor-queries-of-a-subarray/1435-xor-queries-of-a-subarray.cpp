aacd81bbcda584ea0245d1b3f668cea7adcb89f0
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-09-14 13:13:04 +0530

    Time: 72 ms (29.79%), Space: 41.7 MB (71.65%) - LeetHub

---
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& q) {
+        vector<int> v(arr.size());
+        v[0]= arr[0];
+        for(int i=1; i<arr.size(); i++) v[i] = v[i-1]^arr[i];
+
+
+        for(auto i: v) cout<<i<<" ";
+
+        int m = q.size();
+        vector<int> ans(m);
+        for(int i=0; i< m; i++){
+            if(q[i][0] > 0)
+                ans[i] = v[q[i][1]] ^v[q[i][0]-1]; 
+            else ans[i] = v[q[i][1]];
+        }
+        return ans;
+    }
+};
