8385cdcb53f42aab1e2c3979dedcb742e333c3ff
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-10-02 22:40:26 +0530

    Time: 101 ms (77.5%), Space: 73.5 MB (62.5%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -3,9 +3,11 @@ public:
     bool canArrange(vector<int>& arr, int k) {
         unordered_map<int, int> st;
         for(int i: arr) {
+            // printf("%d, %d, %d\n", i % k, (i % k + k), (i % k + k) % k);
             st[(i % k + k) % k]++;  // Ensure non-negative remainders
         }
         for(auto& i : st) {
+            // cout<<i.first<<" "<<i.second<<endl;
             if(i.first == 0) {
                 if(i.second % 2 != 0) return false;
             } else {
