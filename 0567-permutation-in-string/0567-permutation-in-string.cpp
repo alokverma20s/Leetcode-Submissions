c965f0fd75f1e6033b047b1108d16d9a455ab617
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-10-05 22:04:59 +0530

    Time: 8 ms (56.9%), Space: 8.6 MB (98.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length(), m = s2.length();
-        int i=0;
-        sort(s1.begin(), s1.end());
+        if (n > m) return false;
 
-        for(int k = 0; k<= m-n; k++){
-            string temp = s2.substr(i, n);
-            i++;
-            sort(temp.begin(), temp.end());
-            // cout<< s1<<" "<<temp<<endl;
-            if(s1 == temp) return true;
+        vector<int> s1Count(26, 0), s2Count(26, 0);
+        for (int i = 0; i < n; i++) {
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
         }
-        return false;
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] == s2Count[i]) matches++;
+        }
+
+        for (int i = 0; i < m - n; i++) {
+            if (matches == 26) return true;
+
+            int r = s2[i + n] - 'a', l = s2[i] - 'a';
+            s2Count[r]++;
+            if (s2Count[r] == s1Count[r]) matches++;
+            else if (s2Count[r] == s1Count[r] + 1) matches--;
+
+            s2Count[l]--;
+            if (s2Count[l] == s1Count[l]) matches++;
+            else if (s2Count[l] == s1Count[l] - 1) matches--;
+        }
+
+        return matches == 26;
     }
 };
