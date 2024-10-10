34245789f2e29a616522d6636ba79f64ad531d92
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-10-11 03:34:25 +0530

    Time: 59 ms (32.1%), Space: 47 MB (25.09%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int, int>> v(n);
-        for(int i=0; i<n; i++){
-            v[i] = {nums[i], i};
-        }
-        sort(v.begin(), v.end());
-
-        int maxi_width = 0;
-        int mini = v[0].second;
-
-        for(int i =1; i< n; i++){
-            if(v[i].second < mini){
-                mini = v[i].second;
-            }
-            else{
-                maxi_width = max(maxi_width, v[i].second - mini);
+        stack<int> st;
+        st.push(0);
+        for(int i=1; i<n; i++){
+            if(nums[st.top()] > nums[i]){
+                st.push(i);
             }
         }
-        return maxi_width;
+
+        int i = n-1;
+        int ans  =0;
+        while(!st.empty() && i >= 0){
+            if(nums[i] >= nums[st.top()]){
+                ans = max(ans, i-st.top());
+                st.pop();
+            }
+            else i--;
+        }
+        return ans;
     }
 };
