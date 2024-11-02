a092149929a5dd2d6812b694c682ce8fc2ed5650
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-02 21:21:43 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        if(s[0] != s[s.length()-1]) return false;
+
+        for(int i=0; i< s.length(); i++){
+            if(s[i] ==' ' && s[i-1] != s[i+1]) return false;
+        }
+        return true;
+    }
+};
