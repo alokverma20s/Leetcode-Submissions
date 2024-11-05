bf99191fd10196bca8483b384ddc57857709a7aa
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-05 22:21:16 +0530

    Time: 3 ms (12.37%), Space: 9.2 MB (12.33%) - LeetHub

--- 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        unordered_map<char, int> m;
-        for(int i =0; i< s.length();i++){
-            m[s[i]]++;
-        }
-        bool vis[26] = {false};
-        string res ="";
-        for(int i =0; i< s.length(); i++){
-            m[s[i]]--;
-            if(!vis[s[i]-'a']){
-                while (res.size()>0 && res.back()>s[i] && m[res.back()] > 0) {
-                    vis[res.back() - 'a'] = 0;
-                    res.pop_back();
-                }
-                res += s[i];
-                vis[s[i] - 'a'] = 1;
+        unordered_map<char, int> freq;
+        unordered_map<char, bool> visited;
+
+        for(int i: s) freq[i]++, visited[i] = false;
+        stack<char> st;
+
+        for(int i=0; i<s.length(); i++){
+            // cout<<s[i]<<" "<<freq[s[i]]<< " ";
+            while(!st.empty() && st.top() > s[i] && freq[st.top()] > 0 && visited[s[i]] == false){
+                visited[st.top()] = false;
+                // cout<<"Removed "<<st.top()<<" ";
+                st.pop();
             }
+            if(visited[s[i]] == false){
+                // cout<<"Pushed "<<s[i]<<" ";
+                visited[s[i]] = true;
+                st.push(s[i]);
+            }
+            // else cout<<"Skipped "<<s[i]<<" ";
+            // cout<<endl;
+            freq[s[i]]--;
         }
-        return res;
+        string ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
