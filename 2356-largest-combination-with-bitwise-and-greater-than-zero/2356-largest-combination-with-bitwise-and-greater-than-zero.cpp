2cbb9d163dfa4a86770e00d97c969c0eea789176
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-07 09:33:17 +0530

    Time: 9 ms (96.27%), Space: 60.3 MB (32.98%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int largestCombination(vector<int>& c) {
         int n = c.size();
-        vector<bitset<24>> v(n);
+        vector<int> v(24, 0);
 
-        for(int i=0; i< n; i++){
-            bitset<24> temp(c[i]);
-            v[i] = temp;
-        }
-
-        int ans = 0;
-        for(int i=0; i<24; i++){
-            int cnt = 0;
-            for(int j=0; j< n; j++){
-                // cout<< v[j][i]<<" ";
-                if(v[j][i] == 1) cnt++;
+        for(int i: c){
+            int sh = 0;
+            while(i){
+                v[sh] += i & 1;
+                sh++;
+                i >>= 1;
             }
-            // cout<<endl;
-            ans = max(ans, cnt);
         }
-        return ans;
+        return *max_element(v.begin(), v.end());
     }
 };
