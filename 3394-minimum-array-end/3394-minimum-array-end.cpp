ae6920a8e158c5fb0ce6a9425fe414847262654e
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-09 10:47:26 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.07%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -4,15 +4,20 @@ public:
         bitset<64> b(x);
         vector<int> idx(64, 64);
         int j=0;
-        for(int i =0; i < 64; i++)
-            if(b[i] == 0) 
+        for(int i =0; i < 64; i++){
+            if(b[i] == 0)
                 idx[j++] = i;
+        }
 
-        bitset<64> rem(--n);
+        n-=1;
+        bitset<64> rem(n);
 
-        for(int i=0, j = 0; i< 64 && idx[j] < 64; i++)
-            b[idx[j++]]= rem[i];
-        
+        j= 0;
+        for(int i=0; i< 64 && idx[j] < 64; i++){
+            b[idx[j]]= rem[i];
+            j++;
+        }
+
         return b.to_ulong();
     }
 };
