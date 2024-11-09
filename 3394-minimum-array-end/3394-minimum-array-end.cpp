bec6854e921742aca7602562c5da3a81d438b970
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-09 10:44:32 +0530

    Time: 3 ms (31.25%), Space: 9 MB (33.22%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -2,29 +2,17 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         bitset<64> b(x);
-        int mst = 0;
         vector<int> idx(64, 64);
         int j=0;
-        for(int i =0; i < 64; i++){
-            if(b[i] == 1) {
-                mst = i+1;
-            }
-            else{
+        for(int i =0; i < 64; i++)
+            if(b[i] == 0) 
                 idx[j++] = i;
-            }
-        }
-        int unsetbit = mst - b.count();
-        n-=1;
-        bitset<64> rem(n);
-        // cout<<n<<" "<< "rem is "<< rem<<endl;
 
-        j= 0;
-        for(int i=0; i< 64 && idx[j] < 64; i++){
-            // cout<<idx[j]<<" "<<rem[i]<<" ";
-            b[idx[j]]= rem[i];
-            // cout<<b<<" \n";
-            j++;
-        }
+        bitset<64> rem(--n);
+
+        for(int i=0, j = 0; i< 64 && idx[j] < 64; i++)
+            b[idx[j++]]= rem[i];
+        
         return b.to_ulong();
     }
 };
