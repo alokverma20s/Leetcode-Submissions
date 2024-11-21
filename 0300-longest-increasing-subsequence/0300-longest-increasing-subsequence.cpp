30f63a74ea2b357d3cc4b564ad52b2451c2439ab
Alok Verma <94442607+alokverma20s@users.noreply.github.com>
2024-11-21 11:06:36 +0530

    Time: 57 ms (68.61%), Space: 13.2 MB (66.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,13 +3,16 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> dp(nums.size(), 1);
 
+        int ans = 0;
+
         for(int i=0; i< nums.size(); i++){
             for( int prev = 0 ; prev < i; prev++){
                 if(nums[i] > nums[prev]){
                     dp[i] = max(dp[i], (1 + dp[prev]));
                 }
             }
+            ans = max(ans, dp[i]);
         }
-        return *max_element(dp.begin(), dp.end());
+        return ans;
     }
 };
